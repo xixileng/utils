04e18c8110151680054882b873c3675d585eac26
Xiaoleng123 <896054682@qq.com>
2019-01-31 17:22:38 +0800

    Create downloadImageByCanvas.js

---
+++ downloadImageByCanvas.js
@@ -0,0 +1,27 @@
+function downloadImage (img, opts = {}) {
+  const { width, height, name = '下载.png' } = opts;
+
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+
+  canvas.width = img.width;
+  canvas.height = img.height;
+  if (width) {
+    canvas.width = img.width = width;
+  }
+  if (height) {
+    canvas.height = img.height = height;
+  }
+  ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+  const imgUrl = canvas.toDataURL('image/png');
+
+  const link = document.createElement('a');
+  link.href = imgUrl;
+  link.download = name;
+
+  const clickEvent = document.createEvent('MouseEvents');
+  clickEvent.initEvent('click', true, false);
+  link.dispatchEvent(clickEvent);
+}
+
+export default downloadImage;
